1f8cecd55818889145be387692366fbce18277ea
carlosvt777 <carlosvtdev@gmail.com>
2025-12-07 21:56:06 -0300

    Simplify NextAuth config (remove logger)

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,21 +1,15 @@
-import NextAuth from "next-auth"
-import prisma from "./prisma"
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import { Adapter } from "next-auth/adapters"
-import GitHub from "next-auth/providers/github"
-import Google from "next-auth/providers/google"
+import NextAuth from "next-auth";
+import prisma from "./prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { Adapter } from "next-auth/adapters";
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma) as Adapter,
   trustHost: true,
-  debug: true,
-  logger: {
-    error(code, metadata) {
-      console.error("[AUTH ERROR]", code, metadata)
-    },
-  },
   providers: [
     GitHub,
     Google,
   ],
-})
+});
