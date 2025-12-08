6519b63241da038703228beee0261567e49b9512
carlosvt777 <carlosvtdev@gmail.com>
2025-12-07 21:34:21 -0300

    Fix NextAuth Google provider env vars

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,12 +1,21 @@
 import NextAuth from "next-auth"
-import prisma from './prisma'
+import prisma from "./prisma"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { Adapter } from "next-auth/adapters"
 import GitHub from "next-auth/providers/github"
-import Google from 'next-auth/providers/google'
+import Google from "next-auth/providers/google"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma) as Adapter,
   trustHost: true,
-  providers: [GitHub, Google],
+  providers: [
+    GitHub({
+      clientId: process.env.AUTH_GITHUB_ID!,
+      clientSecret: process.env.AUTH_GITHUB_SECRET!,
+    }),
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID!,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+    }),
+  ],
 })
